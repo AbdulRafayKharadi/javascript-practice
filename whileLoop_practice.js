528bbfc62d73c10bac4b6184a26b1dd337785cfe
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-17 19:34:03 +0500

    While Loop Practice Question 10

--- whileLoop_practice.js
+++ whileLoop_practice.js
@@ -93,3 +93,18 @@
             number++;        
         }
 }
+{
+    /* 10. find a bug in this code snippet
+        let i = 1;
+        while (i <= 5)
+            {
+                console.log(i);
+            } */ 
+
+    let i = 1;
+    while (i <= 5)
+        {
+            console.log(i); // 1 2 3 4 5
+            i++; // added this to avoid infinite loop
+        }
+}
