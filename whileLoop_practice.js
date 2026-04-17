518ed692d4279f28483dad08956bfcc88c566b81
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-17 16:11:01 +0500

    While Loop Practice Question 5

--- whileLoop_practice.js
+++ whileLoop_practice.js
@@ -34,3 +34,14 @@
             oddNumber += 2;
         }
 }
+{
+    // 5. Find the sum of numbers from 1 to 10.
+    let number = 1;
+    let sum = 0;
+    while(number <= 10)
+        { 
+            sum += number; 
+            number++;
+        }
+    console.log(sum); // 55
+}
