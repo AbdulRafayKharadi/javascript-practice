a68d62ae38f6177a25f2a91b5e70def4aae34eb2
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-17 16:04:55 +0500

    While Loop Practice Question 2

--- whileLoop_practice.js
+++ whileLoop_practice.js
@@ -1,8 +1,18 @@
 {
     // 1. Print numbers from 1 to 10 using a while loop. 
     let number = 1;
-    while ( number <= 10 ){
-        console.log(number);
-        number++;
+    while ( number <= 10 )
+        {
+            console.log(number); // 1 2 3 4 5 6 7 8 9 10
+            number++;
     } 
+}
+{
+    // 2. Print numbers from 10 to 1 (reverse).
+    let number = 10;
+    while(number >= 1)
+        {
+            console.log(number); // 10 9 8 7 6 5 4 3 2 1
+            number--;
+        }
 }
