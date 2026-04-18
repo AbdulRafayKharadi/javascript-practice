806c5d943f41be0fa9ed78493fed32f7fadb1257
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-18 16:59:01 +0500

    Do-While Loop Practice Question 1

---
+++ doWhileLoop_practice.js
@@ -0,0 +1,10 @@
+{
+    // 1. Print numbers from 1 to 10 using do...while.
+    let number = 1;
+    do
+        {
+            console.log(`${number}`);
+            number++;
+        }
+    while(number <= 10);
+}
