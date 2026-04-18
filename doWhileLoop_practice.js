039815aa97c93247089746591fabbb02d6a55502
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-18 17:51:16 +0500

    Do-While Loop Practice Question 2

--- doWhileLoop_practice.js
+++ doWhileLoop_practice.js
@@ -3,8 +3,18 @@
     let number = 1;
     do
         {
-            console.log(`${number}`);
+            console.log(`${number}`); // 1 2 3 4 5 6 7 8 9 10
             number++;
         }
     while(number <= 10);
+}
+{
+    // 2. Ask user for a number and print it once even if it's invalid.
+    let number;
+    do
+    {
+           number = Number(prompt("Enter a valid number"));
+            console.log(`Your number: ${number}`); // provided number by the user OR NaN
+    }
+    while(isNaN(number));
 }
