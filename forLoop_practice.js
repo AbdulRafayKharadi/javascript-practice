24ee252a3661e1862696b50b9daa122278aca0ed
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-20 18:26:37 +0500

    For Loop Practice Question 2

--- forLoop_practice.js
+++ forLoop_practice.js
@@ -5,3 +5,10 @@
             console.log(i); // 0 1 2 3 4 5 6 7 8 9
         }
 }
+{
+    // 2. Print numbers from 2 to 20, increasing by 2 each time.
+    for(let i = 2; i <= 20; i += 2)
+        {
+            console.log(`Multiple of 2: ${i}`); // 2 4 6 8 10 12 14 16 18 20
+        }
+}
