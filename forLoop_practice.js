9a97cbefe037b5570c21fb80bb26edaee78e265c
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-20 18:29:05 +0500

    For Loop Practice Question 3

--- forLoop_practice.js
+++ forLoop_practice.js
@@ -12,3 +12,10 @@
             console.log(`Multiple of 2: ${i}`); // 2 4 6 8 10 12 14 16 18 20
         }
 }
+{
+    // 3. Print numbers from 50 down to 40.
+    for(let i = 50; i >= 40; i--)
+        {
+            console.log(`i ${i}`); // 50 49 48 47 46 45 44 43 42 41 40
+        }
+}
