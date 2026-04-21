9350eb2ca3c0e22688d7be9e62e44dbd263fe7d5
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-04-21 23:58:26 +0500

    forOf Loop Practice Question 1

---
+++ forOfLoop_practice.js
@@ -0,0 +1,9 @@
+{
+    // 1.Print each value using for…of. 
+    // Given: arr = [10, 20, 30];
+    let arr = [10, 20, 30];
+for( let val of arr)
+    {
+        console.log(`value: ${val}`);
+    }
+}
